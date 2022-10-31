12c12c34374be0d3cb8a2298c1e9245977c8aff9
Dileepa Mihiranga <dileepams@gmail.com>
2022-11-01 00:45:56 +0530

    Add all files

---
+++ scripts/responses.js
@@ -0,0 +1,278 @@
+//Intent Categories
+
+let hi = [
+    "Hello there",
+    "Hi there",
+    "Hey",
+];
+
+let intro = [
+    "I am Roby who is your chatbot",
+    "I am Roby. I can help you to find educational information",
+    "I am Roby. I like to helping you and talking long walks on the beach"
+];
+
+let MyName = [
+    "My name is Roby",
+    "I am Roby"
+];
+
+let greetings = [
+    "I am fine, what about you",
+    "Fine, how about you?",
+    "I am good. how about you?"
+];
+
+let fine = [
+    "How may I assist you?",
+    "How can I help you?",
+    "What I can do for you?"
+];
+
+let WhatCan = [
+    "I can help you to find learning resources"
+]
+
+let purpose = [
+    "The main purpose of the edication is to provide for the fullest possible development of each learner for living morally, creatively, and productively in a democratic society."
+]
+
+let OnlineEdu = [
+    "Transitioning to online studies can be a hard move, especially if you are completely new to online or cloud-based learning."
+]
+
+let hobbies = [
+    "I love to talk with humans",
+    "I like to make friends like you",
+    "I love to learn human languages"
+];
+
+let thank = [
+    "Most welcome",
+    "Not an issue",
+    "My pleasure",
+    "Mention not"
+];
+
+let ChatEnd = [
+    'Ok! bye',
+    'See you later',
+    'Bye, see you soon'
+]
+
+let WebDesign = [
+    'Want learn web design or want to advice to become a Web Designer?',
+    'Do you want to learn web design or are you want to guidence for enter to web design career?',
+]
+let WebDesignLearn = [
+    'Here is some usefull tips to learn web design faster. <br><br> 01. First of all learn HTML, CSS & JavaScript <br><br> 02. Play with Code at Codeacademy <br><br> 03.Pay Attention to Websites You Love <br><br> 04. Learn about CSS frameworks (Bootstrap, TailWind) <br><br> 05. Learn about JS frameworks (Angular, ReactJS, VeuJS) <br><br>06.Get Comfortable with Responsive web design <br><br> 07.Get Comfortable with SEO <br><br> Refer this link also <br> <a href="https://w3schools.com" target="_blank">Learn web design from W3 Schools</a>',
+    'Follow these usefull tips to learn web design within short time period. <br><br> 01. First of all learn HTML, CSS & JavaScript <br><br> 02. Play with Code at Codeacademy <br><br> 03.Pay Attention to Websites You Love <br><br> 04. Learn about CSS frameworks (Bootstrap, TailWind) <br><br> 05. Learn about JS frameworks (Angular, ReactJS, VeuJS) <br><br> 06.Get Comfortable with Responsive web design <br><br> 07.Get Comfortable with SEO <br><br> Refer this link also <br> <a href="https://w3schools.com" target="_blank">Learn web design from W3 Schools</a>',
+]
+let WebDesignCourse = [
+    'Follow these links to learn web design online. <br> <a href="https://w3schools.com" target="_blank">Learn zero to hero from W3 Schools<a> <br> <a href="https://cutt.ly/jnewRJ5" target="_blank">Alison free web design course<a> <br> <a href="https://cutt.ly/bnewmNC" target="_blank"> Webflow University <a> <br> <a href="https://cutt.ly/0newuek" target="_blank"> LearnCode.academy YouTube playlist <a> <br> <a href="https://cutt.ly/kned4KU" target="_blank"> Web Design for Beginners - Udemy.com<a>',
+    'Use these online resourses to learn web design. <br> <a href="https://w3schools.com" target="_blank">Learn zero to hero from W3 Schools<a> <br> <a href="https://cutt.ly/jnewRJ5" target="_blank">Alison free web design course<a> <br> <a href="https://cutt.ly/bnewmNC" target="_blank"> Webflow University <a> <br> <a href="https://cutt.ly/0newuek" target="_blank"> LearnCode.academy YouTube playlist <a> <br> <a href="https://cutt.ly/kned4KU" target="_blank"> Web Design for Beginners - Udemy.com <a>',
+]
+let WebDesignBecome = [
+    'Here is some tips about how to become a web designer <br> 01. Understand the role <br> 02. Determine if web design is the right career for you <br> 03. Learn the skills and tools you need on the job <br> 04.Learn the business skills and tools you need for freelancing <br> 05. Find inspiration and connections in the community <br> 06. Take your first step toward becoming a web designer <br><br> Use these links to learn more about how to beacome a web designer <br> <a href="https://cutt.ly/kne5oU4">How to become a web designer in 2020</a> <br> <a href="https://cutt.ly/Jne5vAg">10 Best Steps Guide on How to Become a Web Designer</a>',
+]
+let WebDesignMoney = [
+    'You can earn money as a web designer from following ways <br> 01. Work for a Design Agency <br> 02. Work as an In-House Designer <br> 03. Freelance <br> 04.Start Your Own Agency <br> 05. Start a Design Blog <br> 06. Create a Course <br><br> Use this links to learn more about how to earn money as a web designer <br> <a href="https://cutt.ly/Sne6VyL">9 Ways to Make Money as a Web Designer</a>',
+]
+
+
+let WebDevelop = [
+    'Want learn web development or want to advice to become a web developer?',
+    'Do you want to learn web development or are you want to guidence for enter to web development career?',
+]
+let WebDevelopLearn = [
+    'Here is some usefull tips to learn web design faster. <br><br> 01. Learn the Basics of HTML, CSS, and Javascript <br><br> 02. Get to Know UI and UX <br><br> 03.Learn SQL and PHP <br><br> 04. Learn the Basics of SEO <br><br> 05. Make sure the site is responsive <br><br> Use this link to learn web development free <br> <a href="https://cutt.ly/znrucrI" target="_blank">Free Web Development Courses by Udemy<a>',
+    'Follow these usefull tips to learn web design within short time period. <br><br> 01. Learn the Basics of HTML, CSS, and Javascript <br><br> 02. Get to Know UI and UX <br><br> 03.Learn SQL and PHP <br><br> 04. Learn the Basics of SEO <br><br> 05. Make sure the site is responsive <br><br> Use this link to learn web development free <br> <a href="https://cutt.ly/znrucrI" target="_blank">Free Web Development Courses by Udemy<a>',
+]
+let WebDevelopCourse = [
+    'Follow these links to learn web development online. <br> <a href="https://cutt.ly/8nrupJE" target="_blank">Web Development Courses by Coursera<a> <br> <a href="https://cutt.ly/znrucrI" target="_blank">Free Web Development Courses by Udemy<a> <br> <a href="https://cutt.ly/cnruDgO" target="_blank"> Learn web development by Mozilla Developer Network <a>',
+    'Use these online resourses to learn web development. <br> <a href="https://cutt.ly/8nrupJE" target="_blank">Web Development Courses by Coursera<a> <br> <a href="https://cutt.ly/znrucrI" target="_blank">Free Web Development Courses by Udemy<a> <br> <a href="https://cutt.ly/cnruDgO" target="_blank"> Learn web development by Mozilla Developer Network <a>',
+]
+let WebDevelopBecome = [
+    'Here is some tips about how to become a web developer <br> 01. Build a Portfolio Site <br> 02. Do Freelance Projects <br> 03. Put Your Code on GitHub <br> 04.Network with Other Web Developers <br> 05. Participate in a Hackathon',
+]
+let WebDevelopMoney = [
+    'You can earn money as a web designer from following ways <br> 01. Reach Out to Local Small Businesses <br> 02. build a SAAS (Software as a Service) <br> 03. Freelance <br> 04.Create a blog and monetize it <br> 05. create a web development YouTube channel <br> 06. participate in paid coding challenges',
+]
+
+
+
+//Input Categories
+function getBotResponse(input) {
+
+    //hi
+    if(input.toLowerCase().includes('hi')){
+        return hi[Math.floor(Math.random() * hi.length)];
+    }
+    if(input.toLowerCase().includes('hello')){
+        return hi[Math.floor(Math.random() * hi.length)];
+    }
+    if(input.toLowerCase().includes('hey')){
+        return hi[Math.floor(Math.random() * hi.length)];
+    }
+
+    //intro
+    if(input.toLowerCase().includes('who are you')){
+        return intro[Math.floor(Math.random() * intro.length)];
+    }
+    if(input.toLowerCase().includes('may i know who you are?')){
+        return intro[Math.floor(Math.random() * intro.length)];
+    }
+
+    //name
+    if(input.toLowerCase().includes('what is your name?')){
+        return MyName[Math.floor(Math.random() * MyName.length)];
+    }
+    if(input.toLowerCase().includes('do you have a name')){
+        return MyName[Math.floor(Math.random() * MyName.length)];
+    }
+
+    //greetings
+    if(input.toLowerCase().includes('how are you')){
+        return greetings[Math.floor(Math.random() * greetings.length)];
+    }
+    if(input.toLowerCase().includes("what's up")){
+        return greetings[Math.floor(Math.random() * greetings.length)];
+    }
+    if(input.toLowerCase().includes("what's going on")){
+        return greetings[Math.floor(Math.random() * greetings.length)];
+    }
+    if(input.toLowerCase().includes("how's it going?")){
+        return greetings[Math.floor(Math.random() * greetings.length)];
+    }
+
+    //fine
+    if(input.toLowerCase().includes('fine')){
+        return fine[Math.floor(Math.random() * fine.length)];
+    }
+    if(input.toLowerCase().includes('i am good')){
+        return fine[Math.floor(Math.random() * fine.length)];
+    }
+    if(input.toLowerCase().includes('pretty good')){
+        return fine[Math.floor(Math.random() * fine.length)];
+    }
+
+    //WhatCan
+    if(input.toLowerCase().includes('what can you do')){
+        return WhatCan[Math.floor(Math.random() * WhatCan.length)];
+    }
+    if(input.toLowerCase().includes('can you help')){
+        return WhatCan[Math.floor(Math.random() * WhatCan.length)];
+    }
+
+    //purpose
+    if(input.toLowerCase().includes('purpose of education')){
+        return purpose[Math.floor(Math.random() * purpose.length)];
+    }
+
+    //OnlineEdu
+    if(input.toLowerCase().toLowerCase().includes('learn during covid 19')){
+        return OnlineEdu[Math.floor(Math.random() * OnlineEdu.length)];
+    }
+    if(input.toLowerCase().toLowerCase().includes('learn during pandemic')){
+        return OnlineEdu[Math.floor(Math.random() * OnlineEdu.length)];
+    }
+    if(input.toLowerCase().toLowerCase().includes('learn during the covid 19')){
+        return OnlineEdu[Math.floor(Math.random() * OnlineEdu.length)];
+    }
+    if(input.toLowerCase().toLowerCase().includes('learn during the pandemic')){
+        return OnlineEdu[Math.floor(Math.random() * OnlineEdu.length)];
+    }
+    if(input.toLowerCase().toLowerCase().includes('online learn')){
+        return OnlineEdu[Math.floor(Math.random() * OnlineEdu.length)];
+    }
+    if(input.toLowerCase().toLowerCase().includes('online education')){
+        return OnlineEdu[Math.floor(Math.random() * OnlineEdu.length)];
+    }
+
+    //hobbies
+    if(input.toLowerCase().includes('what is your hobby')){
+        return hobbies[Math.floor(Math.random() * hobbies.length)];
+    }
+    if(input.toLowerCase().includes('you have a hobby')){
+        return hobbies[Math.floor(Math.random() * hobbies.length)];
+    }
+    if(input.toLowerCase().includes('you have any hobbies')){
+        return hobbies[Math.floor(Math.random() * hobbies.length)];
+    }
+
+    //thank
+    if(input.toLowerCase().includes('thank')){
+        return thank[Math.floor(Math.random() * thank.length)];
+    }
+
+    //ChatEnd
+    if(input.toLowerCase().includes('bye')){
+        return ChatEnd[Math.floor(Math.random() * ChatEnd.length)];
+    }
+    if(input.toLowerCase().includes('see you')){
+        return ChatEnd[Math.floor(Math.random() * ChatEnd.length)];
+    }
+
+    //WebDesign
+    if(input.toLowerCase().includes('web design')){
+        //WebDesignLearn
+        if(input.toLowerCase().includes('learn')){
+            return WebDesignLearn[Math.floor(Math.random() * WebDesignLearn.length)];
+        }
+        //WebDesignCourse
+        else if (input.toLowerCase().includes('course')){
+            return WebDesignCourse[Math.floor(Math.random() * WebDesignCourse.length)];
+        }
+        //WebDesignBecome
+        else if (input.toLowerCase().includes('be')){
+            return WebDesignBecome[Math.floor(Math.random() * WebDesignBecome.length)];
+        }
+        else if (input.toLowerCase().includes('career')){
+            return WebDesignBecome[Math.floor(Math.random() * WebDesignBecome.length)];
+        }
+        //WebDesignMoney
+        else if (input.toLowerCase().includes('earn money')){
+            return WebDesignMoney[Math.floor(Math.random() * WebDesignMoney.length)];
+        }
+        else if (input.toLowerCase().includes('make money')){
+            return WebDesignMoney[Math.floor(Math.random() * WebDesignMoney.length)];
+        }
+        else{
+            return WebDesign[Math.floor(Math.random() * WebDesign.length)];
+        }
+    }
+
+    //WebDevelop
+    if(input.toLowerCase().includes('web develop')){
+        //WebDevelopLearn
+        if(input.toLowerCase().includes('learn')){
+            return WebDevelopLearn[Math.floor(Math.random() * WebDevelopLearn.length)];
+        }
+        //WebDevelopCourse
+        else if (input.toLowerCase().includes('course')){
+            return WebDevelopCourse[Math.floor(Math.random() * WebDevelopCourse.length)];
+        }
+        //WebDevelopBecome
+        else if (input.toLowerCase().includes('be')){
+            return WebDevelopBecome[Math.floor(Math.random() * WebDevelopBecome.length)];
+        }
+        else if (input.toLowerCase().includes('career')){
+            return WebDevelopBecome[Math.floor(Math.random() * WebDevelopBecome.length)];
+        }
+        //WebDevelopMoney
+        else if (input.toLowerCase().includes('earn money')){
+            return WebDevelopMoney[Math.floor(Math.random() * WebDevelopMoney.length)];
+        }
+        else if (input.toLowerCase().includes('make money')){
+            return WebDevelopMoney[Math.floor(Math.random() * WebDevelopMoney.length)];
+        }
+        else{
+            return WebDevelop[Math.floor(Math.random() * WebDevelop.length)];
+        }
+    }
+    
+    else {
+        window.open('https://www.google.com/search?q=' + input);
+        return "Sorry! Did not get it. Still I am learning humans' languages. But I provide a Google search related to your question.";
+    }
+}
